3a5e02e2f7864b9e1ea4f8d49ce69d23addbcc5a
satikaj <117552851+satikaj@users.noreply.github.com>
2025-01-27 16:37:09 +1100

    fix: feedback picker search

--- src/app/tasks/task-comment-composer/task-feedback-templates/task-feedback-templates.component.ts
+++ src/app/tasks/task-comment-composer/task-feedback-templates/task-feedback-templates.component.ts
@@ -9,7 +9,7 @@ import {
   ViewEncapsulation,
   EventEmitter,
 } from '@angular/core';
-import {combineLatest, map, Observable} from 'rxjs';
+import {BehaviorSubject, combineLatest, map, Observable} from 'rxjs';
 import {LearningOutcome, FeedbackTemplate, Task} from 'src/app/api/models/doubtfire-model';
 import {FeedbackTemplateService} from 'src/app/api/services/feedback-template.service';
 import {LearningOutcomeService} from 'src/app/api/services/learning-outcome.service';
@@ -24,11 +24,40 @@ export class TaskFeedbackTemplatesComponent implements OnChanges {
   @Input() task: Task;
   @Output() templateSelected = new EventEmitter<FeedbackTemplate>();
   categories = ['TLO', 'ULO', 'GLO'];
-  searchTerm: string = '';
   hoveredTemplate: FeedbackTemplate;
 
   public tlos$: Observable<{outcome: LearningOutcome; templates: FeedbackTemplate[]}[]>;
   public ulos$: Observable<{outcome: LearningOutcome; templates: FeedbackTemplate[]}[]>;
+  public glos$ = combineLatest([
+    this.learningOutcomeService.cache.values,
+    this.feedbackTemplateService.cache.values,
+  ]).pipe(
+    map(([outcomes, templates]) =>
+      outcomes
+        .filter((outcome) => outcome.contextType === null)
+        .map((outcome) => ({
+          outcome: outcome,
+          templates: templates.filter((template) => template.learningOutcomeId === outcome.id),
+        })),
+    ),
+  );
+
+  private searchTermSubject = new BehaviorSubject<string>('');
+  searchTerm$ = this.searchTermSubject.asObservable();
+  public filteredTlos$: Observable<{outcome: LearningOutcome; templates: FeedbackTemplate[]}[]>;
+  public filteredUlos$: Observable<{outcome: LearningOutcome; templates: FeedbackTemplate[]}[]>;
+  public filteredGlos$ = combineLatest([this.glos$, this.searchTerm$]).pipe(
+    map(([glos, searchTerm]) =>
+      glos
+        .map((glo) => ({
+          ...glo,
+          templates: glo.templates.filter((template) =>
+            template.chipText.toLowerCase().includes(searchTerm.toLowerCase()),
+          ),
+        }))
+        .filter((glo) => glo.templates.length > 0),
+    ),
+  );
 
   constructor(
     private learningOutcomeService: LearningOutcomeService,
@@ -48,6 +77,19 @@ export class TaskFeedbackTemplatesComponent implements OnChanges {
           })),
         ),
       );
+
+      this.filteredTlos$ = combineLatest([this.tlos$, this.searchTerm$]).pipe(
+        map(([tlos, searchTerm]) =>
+          tlos
+            .map((tlo) => ({
+              ...tlo,
+              templates: tlo.templates.filter((template) =>
+                template.chipText.toLowerCase().includes(searchTerm.toLowerCase()),
+              ),
+            }))
+            .filter((tlo) => tlo.templates.length > 0),
+        ),
+      );
     }
 
     if (this.task.unit) {
@@ -62,22 +104,25 @@ export class TaskFeedbackTemplatesComponent implements OnChanges {
           })),
         ),
       );
+
+      this.filteredUlos$ = combineLatest([this.ulos$, this.searchTerm$]).pipe(
+        map(([ulos, searchTerm]) =>
+          ulos
+            .map((ulo) => ({
+              ...ulo,
+              templates: ulo.templates.filter((template) =>
+                template.chipText.toLowerCase().includes(searchTerm.toLowerCase()),
+              ),
+            }))
+            .filter((ulo) => ulo.templates.length > 0),
+        ),
+      );
     }
   }
 
-  public glos$ = combineLatest([
-    this.learningOutcomeService.cache.values,
-    this.feedbackTemplateService.cache.values,
-  ]).pipe(
-    map(([outcomes, templates]) =>
-      outcomes
-        .filter((outcome) => outcome.contextType === null)
-        .map((outcome) => ({
-          outcome: outcome,
-          templates: templates.filter((template) => template.learningOutcomeId === outcome.id),
-        })),
-    ),
-  );
+  onSearchTermChange(searchTerm: string): void {
+    this.searchTermSubject.next(searchTerm);
+  }
 
   @ViewChild('tloSection') tloSection!: ElementRef;
   @ViewChild('uloSection') uloSection!: ElementRef;
